c4df9ea57437483596905ba835dc11aef74e672b
Peter <57006688+TheOfficialPeter@users.noreply.github.com>
2022-12-29 13:20:33 +0000

    Update universalGunFrameworkClient.lua

--- universalGunFrameworkClient.lua
+++ universalGunFrameworkClient.lua
@@ -1,11 +1,15 @@
-function castRays()
+local userInputService = game:GetService("UserInputService")
 
+local function reload(currentAmmo, requestedAmmo)
+    if requestedAmmo < 1000 then
+        -- overwrite ammo
+    end
 end
 
-function shoot()
-
+local function input(input, processed)
+    if input.KeyCode == Enum.KeyCode.R then
+        reload(0, 20)
+    end
 end
 
-function reload()
-
-end
+userInputService.InputBegan:Connect(input)
